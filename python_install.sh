9e0444472f69f00da6194a3a4fa8b1a1d92de595
Andrey Antonov <a.antonov@topadvert.ru>
2024-09-17 14:07:33 +0400

    Install scripts updated for ubuntu 24.04

--- python_install.sh
+++ python_install.sh
@@ -2,7 +2,4 @@
 
 sudo apt update
 sudo apt dist-upgrade -y
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
-sudo apt update
-sudo apt install -y default-libmysqlclient-dev build-essential pkg-config mysql-client python3-pip python3.11 python3.11-dev wget
+sudo apt install -y default-libmysqlclient-dev build-essential pkg-config mysql-client python3-pip python3.12 python3.12-dev software-properties-common wget
--- venv_create.sh
+++ venv_create.sh
@@ -1,17 +1,15 @@
 #! /bin/bash -e
 
-python3.11 -m pip install virtualenv
+python3 -m pip install virtualenv
 if [ -d "venv" ];
 then
   sudo rm -rf venv
 fi
 sudo mkdir venv
 sudo chown -R $USER:$USER ./
-python3.11 -m virtualenv venv
+python3 -m virtualenv venv
 source venv/bin/activate
 pip install -U pip setuptools wheel
 pip install -r requirements.txt
 
 mkdir -p log
-wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O mysql.crt
-chmod 0600 mysql.crt
